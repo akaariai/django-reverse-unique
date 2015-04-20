a3c0f586bcfaad4d445957ff9991d9daa264a114
Anssi Kääriäinen <akaariai@gmail.com>
2015-04-20 13:44:52 +0300

    Removed middlewares from testproject

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -38,12 +38,6 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'testproject.urls'
