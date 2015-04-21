079cc78f3afad8e502748dd00effe669ae33989a
Anssi Kääriäinen <akaariai@gmail.com>
2015-04-21 09:04:58 +0300

    Moved a test to correct class

--- reverse_unique/tests.py
+++ reverse_unique/tests.py
@@ -122,29 +122,6 @@ class ReverseUniqueTests(TestCase):
         with self.assertNumQueries(1):
             self.assertEqual(a1.active_translation.body, "Lalalalala")
 
-
-class FormsTests(TestCase):
-    # ForeignObjects should not have any form fields, currently the user needs
-    # to manually deal with the foreignobject relation.
-    class ArticleForm(forms.ModelForm):
-        class Meta:
-            model = Article
-            fields = '__all__'
-
-    def test_foreign_object_form(self):
-        # A very crude test checking that the non-concrete fields do not get form fields.
-        form = FormsTests.ArticleForm()
-        self.assertIn('id_pub_date', form.as_table())
-        self.assertNotIn('active_translation', form.as_table())
-        form = FormsTests.ArticleForm(data={'pub_date': str(datetime.date.today())})
-        self.assertTrue(form.is_valid())
-        a = form.save()
-        self.assertEqual(a.pub_date, datetime.date.today())
-        form = FormsTests.ArticleForm(instance=a, data={'pub_date': '2013-01-01'})
-        a2 = form.save()
-        self.assertEqual(a.pk, a2.pk)
-        self.assertEqual(a2.pub_date, datetime.date(2013, 1, 1))
-
     def test_reservations(self):
         today = datetime.date.today()
         g1 = Guest.objects.create(name="John")
@@ -174,6 +151,29 @@ class FormsTests(TestCase):
         self.assertQuerysetEqual(
             Room.objects.exclude(current_reservation__guest=g1).order_by('pk'),
             [room2, room3], lambda x: x)
+
+
+class FormsTests(TestCase):
+    # ForeignObjects should not have any form fields, currently the user needs
+    # to manually deal with the foreignobject relation.
+    class ArticleForm(forms.ModelForm):
+        class Meta:
+            model = Article
+            fields = '__all__'
+
+    def test_foreign_object_form(self):
+        # A very crude test checking that the non-concrete fields do not get form fields.
+        form = FormsTests.ArticleForm()
+        self.assertIn('id_pub_date', form.as_table())
+        self.assertNotIn('active_translation', form.as_table())
+        form = FormsTests.ArticleForm(data={'pub_date': str(datetime.date.today())})
+        self.assertTrue(form.is_valid())
+        a = form.save()
+        self.assertEqual(a.pub_date, datetime.date.today())
+        form = FormsTests.ArticleForm(instance=a, data={'pub_date': '2013-01-01'})
+        a2 = form.save()
+        self.assertEqual(a.pk, a2.pk)
+        self.assertEqual(a2.pub_date, datetime.date(2013, 1, 1))
 
 
 class InheritanceTests(TestCase):
